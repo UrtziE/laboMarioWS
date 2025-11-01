0659a3572fd5709db19dac143e02df4edc2d1829
Urtzi Etxegarai <urtzift13@gmail.com>
2025-11-01 11:43:30 +0100

    fistCommit

--- js/Main.js
+++ js/Main.js
@@ -1,6 +1,9 @@
+import { SpriteSheet } from "./SpriteSheet.js"
 let imario=0
 let ien=0
+
 function hasiera(){
+  
 let spriteSheet
 let ohiala= document.getElementById("oihala")
 let context= ohiala.getContext('2d')
@@ -68,45 +71,3 @@ loadImage("https://i1.wp.com/aav255.brandon.courses/img/css-sprites/sprite-super
     })
 
   }
-
-class SpriteSheet {
-
-    constructor(image, width, height){
-        this.image = image;
-        this.width = width;
-        this.height = height;
-        this.tiles = new Map();
-    }
-
-    define(name, x, y, width, height){
-       const buffer = document.createElement('canvas');
-        buffer.width = width;
-        buffer.height = height;
-        buffer
-            .getContext('2d')
-            .drawImage(
-                this.image, 
-                x , 
-                y , 
-                width, 
-                height,
-                0,
-                0,
-                width,
-                height);
-        this.tiles.set(name, buffer);
-    }
-
-    defineTile(name, x, y){
-        this.define(name, x*this.width, y*this.height , this.width, this.height);
-    }
-
-    draw(name, context, x, y){
-        const buffer = this.tiles.get(name);
-        context.drawImage(buffer, x, y);
-    }
-
-    drawTile(name, context, x, y) {
-        this.draw(name, context, x*this.width, y*this.height);
-    }
-}
